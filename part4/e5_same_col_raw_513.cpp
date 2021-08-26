f78bfba011ac7b5b19bc7e09c037cdeb95f3a5f4
Lanskask <grey.wooden.tractor@gmail.com>
2021-08-26 14:45:15 +0300

    Templates for `part5` Begin `e5_same_col_raw_513.cpp`

--- part4/e5_same_col_raw_513.cpp
+++ part4/e5_same_col_raw_513.cpp
@@ -16,9 +16,50 @@
  * */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+//int[4][4] translate_matrix() {
+//    return int[4][4]{};
+//}
+
+vector<vector<int>> translate_matrix(vector<vector<int>> matrix) {
+    vector<vector<int>> translated_matrix;
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = 0; j < matrix.size(); ++j) {
+            cout << matrix[i][j] << ", ";
+        }
+        cout << endl;
+    }
+    return translated_matrix;
+}
+
+vector<vector<int>> translate_matrix2(vector<vector<int>> &matrix) {
+    vector<vector<int>> translated_matrix;
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = 0; j < matrix.size(); ++j) {
+            cout << matrix[j][i] << ", ";
+        }
+        cout << endl;
+    }
+    return translated_matrix;
+}
+
+void check_1() {
+    vector<vector<int>> matrix = {
+            {2, 2, 8, 1},
+            {4, 2, 7, 3},
+            {1, 8, 3, 4},
+            {1, 1, 2, 7},
+    };
+
+    translate_matrix(matrix);
+    translate_matrix2(matrix);
+}
+
 int main() {
+    check_1();
+
     return 0;
 }
